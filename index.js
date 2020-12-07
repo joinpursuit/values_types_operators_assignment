7a74dc262d01ccf7cafc5e06def2e685fc63b59e
cassidybeni <cassidybeni@pursuit.org>
2020-12-07 14:36:35 -0500

    Complete problem 1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Problem 1
 
+// Number evaulates to 17.
+// Number evaluate to 11.
+// Number evaluates to 1.25.
+// Boolean evaluates to true.
+// Boolean evaluates to false.
+// Boolean evaluates to true.
+// Boolean evaluates to false.
+// Number evaluates to 2.
+// String evaluates to "ab".
